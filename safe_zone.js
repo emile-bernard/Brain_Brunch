a9fd61f3a289609ae26e565fdb9db02f87783444
Emile555 <emile.bernard.2@ulaval.ca>
2018-12-23 13:01:03 -0500

    Cleaned up code

--- safe_zone.js
+++ safe_zone.js
@@ -1,7 +1,5 @@
 function preloadSafeZone() {
     safeZoneImg = loadImage('./assets/image/resized_bus.png');
-
-
 }
 
 function setupSafeZone() {
@@ -12,8 +10,6 @@ function setupSafeZone() {
         width, height/2-130,
         50, height/2-20);
     safeZone.shapeColor = color('#3ecc57');
-
     safeZone.addImage(safeZoneImg);
-
     safeZones.add(safeZone);
 }
